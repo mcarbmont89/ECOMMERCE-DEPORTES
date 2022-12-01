d202742f3c4130933cab1207cff17062f4f2f4f2
Gonzalo Garcia <ggarciabusca@gmail.com>
2022-12-01 16:38:04 -0300

    creado modelos en app productos y superusuario

--- ecommerce/productos/models.py
+++ ecommerce/productos/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Deporte(models.Model):
+    nombre = models.CharField(max_length=50)
+
+class Articulo(models.Model):
+    deporte = models.ForeignKey(Deporte, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=50)
+    descripcion = models.TextField()
+    precio = models.DecimalField(decimal_places=2,max_digits=10,)
+    publicacion = models.DateField()
