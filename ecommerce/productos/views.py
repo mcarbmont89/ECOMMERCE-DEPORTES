e93ee4a50784be557ca0901b214c693eb7dc4ffd
Gonzalo Garcia <ggarciabusca@gmail.com>
2022-12-10 23:38:06 -0300

    Editar articulos

--- ecommerce/productos/views.py
+++ ecommerce/productos/views.py
@@ -13,7 +13,7 @@ def inicio(request):
 @login_required
 def articulos_listar(request):
     #obtengo el listado de articulos de la base de datos
-    lista_articulos = Articulo.objects.all()
+    lista_articulos = Articulo.objects.all().order_by("nombre")
     contexto = {"avatar":avatar_usuario(request.user),"articulos_resultado":lista_articulos}
 
     return render(request,"productos/articulos_listar.html", contexto)
@@ -45,7 +45,7 @@ def articulo_nuevo(request):
         deporte_instancia = Deporte.objects.filter(nombre=deporte_nuevo)[0]
         marca_instancia = Marca.objects.filter(nombre=marca_nuevo)[0]
 
-        #creo una instancia llamada "persona_nueva" de la clase "Persona" con los atributos que traigo desde el formulario
+        #creo una instancia llamada "articulo_nuevo" de la clase "Articulo" con los atributos que traigo desde el formulario
         articulo_nuevo = Articulo(deporte=deporte_instancia, nombre=nombre_nuevo,marca=marca_instancia, descripcion=descripcion_nuevo, precio=precio_nuevo, publicacion=publicacion_nuevo, imagen=imagen_nuevo)
         articulo_nuevo.save() #con esto lo guardo en la base de datos
         return redirect("productos-inicio")
@@ -54,3 +54,25 @@ def articulo_nuevo(request):
     deportes = Deporte.objects.all()
     return render(request, "productos/articulo_nuevo.html",{"marcas":marcas,"deportes":deportes})
 
+def articulos_eliminar(request, id_a_eliminar):
+    articulo_borrar = Articulo.objects.get(id=id_a_eliminar)
+    articulo_borrar.delete()
+
+    return redirect("articulos-listar")
+
+def articulos_editar(request, id_a_editar):
+    articulo_editar = Articulo.objects.get(id=id_a_editar)
+
+    if request.method == "POST":
+        articulo_editar.nombre = request.POST["nombre"]
+        articulo_editar.descripcion = request.POST["descripcion"]
+        articulo_editar.precio = request.POST["precio"]
+        articulo_editar.publicacion = request.POST["publicacion"]
+        
+        articulo_editar.save() #con esto guardo los cambios en la base de datos
+        return redirect("articulos-listar")
+    else:
+        datos_inicio = {"inicial":articulo_editar}
+        return render (request,"productos/articulo_editar.html",datos_inicio)
+
+    
